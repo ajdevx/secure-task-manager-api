2a9e4add2211090f07ffa95b4a127061db34cfea
Anurag Jha <abhishekjha102622@gmail.com>
2026-02-19 21:23:15 +0530

    basic user controller logic

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -4,7 +4,7 @@ import {ApiError} from "../utils/ApiError.js"
 
 const registerUser = asyncHandler(async (req,res) =>{
 
-const {fullName, password, email,avatar} =  req.body;
+const {fullName, password, email} =  req.body;
 //validating all necessay fields
 if (
     !fullName?.trim() ||
@@ -25,6 +25,15 @@ const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$
 if(!passwordRegex.test(password)){
     throw new ApiError(400,"Password must be at least 6 characters long and include uppercase, lowercase, number and special character")
 }
+
+//check if user already exist
+const existingUser = await User.findOne({email});
+if(existingUser) throw new ApiError(400, "User already exist");
+//take image path from multer middleware
+const avatarLocalPath = req.file?.path;
+//uploading avatar to cloudinary
+const avatar = await uploadOnCloudinary(avatarLocalPath);
+
 
 
 
